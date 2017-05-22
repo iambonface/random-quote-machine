82a819a20713cffba3f09f38767f59f35b87c2ad
Bonface Mudiri <mudirib@gmail.com>
2017-05-22 20:07:27 +0300

    Create jQuery Ajax GET request in js/quote-machine-script.js

--- js/quote-machine-script.js
+++ js/quote-machine-script.js
@@ -5,11 +5,37 @@
  * Web: https://www.linkedin.com/in/bonface/
  */
 
-
- $(function(){
-
  	$(document).ready(function(){
- 		
- 	});
 
- })();
+ 		function getQuoteAPI(){
+
+ 			var root = 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous&count=1';
+
+ 			$.ajax({
+
+ 				headers:{
+ 					"X-Mashape-Key": "FNEeUmn6UTmshk5zlX0dMecVnNC8p18yke7jsnqjVeRNRvCo0p",
+ 					"Content-Type": "application/x-www-form-urlencoded",
+ 					"Accept": "application/json"
+
+ 				},
+ 				url: root,
+ 				dataType: 'json',
+ 				method: 'GET',
+ 				success: function(data){
+
+ 					$(data).each(function(index, value){
+ 						console.log(value.quote);
+ 					});
+ 				}
+ 			});
+
+
+ 		}
+
+ 		$("#btnQuote").click(function(){
+
+ 			getQuoteAPI();
+
+ 		});
+ 	});
