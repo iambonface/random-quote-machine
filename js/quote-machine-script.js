0b3e728f3e8a89c053e6ad8d8e4783230b40405c
Bonface Mudiri <mudirib@gmail.com>
2017-05-22 19:44:39 +0300

    Add js/jquery-min.js and create ready()

---
+++ js/quote-machine-script.js
@@ -0,0 +1,15 @@
+/**
+ * @file Random Quote Machine Script; A freeCodeCamp Project
+ * @author Bonface Mudiri 
+ * Email: tastywebapps@gmail.com
+ * Web: https://www.linkedin.com/in/bonface/
+ */
+
+
+ $(function(){
+
+ 	$(document).ready(function(){
+ 		
+ 	});
+
+ })();
